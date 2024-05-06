fe17fbefcd8e50c4cbc9f0ac93bd13ff17fe8239
luca.cantagallo <Luca.cantagallo@dechit.it>
2024-05-06 11:41:24 +0200

    Start

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    java
+    application
+}
+repositories { // Where to search for dependencies
+    mavenCentral()
+}
+
+application {
+    mainClass.set("Luca")
+}
---
+++ src/main/java/Luca.java
@@ -0,0 +1,5 @@
+public class Luca {
+    public static void main(String[] args) {
+        System.out.println("Hello world, Luca's work");
+    }
+}
